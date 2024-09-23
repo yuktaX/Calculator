0faa1744c7a07e3c56087ff756b7f8a456e39a5c
yuktaX <yukta.rajapur@gmail.com>
2024-09-23 23:06:08 +0530

    testing 1555

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -12,7 +12,7 @@ public class Main {
         Scanner myObj = new Scanner(System.in);
 
         //testing deployment-BJDSLJIBFLSIBF
-        //FF
+        //FF wtethwttirngrgrengithithnitniythnryit
 
         while(true) {
             System.out.println("||---Welcome to Calculator---||");
