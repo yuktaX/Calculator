7def1fcf042f9ad2c13a442251c212d5ed92d36b
yuktaX <yukta.rajapur@gmail.com>
2024-09-23 14:57:27 +0530

    testing deployment -1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -11,7 +11,7 @@ public class Main {
         logger.info("Start of Execution");
         Scanner myObj = new Scanner(System.in);
 
-        //testing deployment
+        //testing deployment-1
 
         while(true) {
             System.out.println("||---Welcome to Calculator---||");
